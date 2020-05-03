7da11d73cdd4c7c2365831b0e4898a597c24d154
asjadanis <asjadanis128@gmail.com>
2020-05-04 03:21:30 +0500

    Updates

---
+++ precache-manifest.54ce4feeab02b6f7361bb8c3f8965260.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f401194eaef63d879322a3d0f45b87",
+    "url": "/react-redux-boilerplate/index.html"
+  },
+  {
+    "revision": "9c21c6c2ecda733f3305",
+    "url": "/react-redux-boilerplate/static/css/2.48a77e21.chunk.css"
+  },
+  {
+    "revision": "878ebf2a493a31fc5248",
+    "url": "/react-redux-boilerplate/static/css/main.b34a28a4.chunk.css"
+  },
+  {
+    "revision": "9c21c6c2ecda733f3305",
+    "url": "/react-redux-boilerplate/static/js/2.579ade51.chunk.js"
+  },
+  {
+    "revision": "a9dd647aec81fffade4fa7e6d693dad7",
+    "url": "/react-redux-boilerplate/static/js/2.579ade51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "878ebf2a493a31fc5248",
+    "url": "/react-redux-boilerplate/static/js/main.4ed96ce6.chunk.js"
+  },
+  {
+    "revision": "8522807c7a72ea477db8",
+    "url": "/react-redux-boilerplate/static/js/runtime-main.ca062428.js"
+  }
+]);
